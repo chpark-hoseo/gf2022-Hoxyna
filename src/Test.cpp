dd4698c2e9033b717046fd43b4c0ab0f201886a6
Hoxyna <frankim0622@naver.com>
2022-09-16 13:44:52 +0900

    HelloSDL-분리하기 실습, 과제 완료

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,30 +1,78 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <time.h>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
+void input();
+void update();
+void render();
 
 int main(int argc, char* args[])
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+	srand(time(NULL));
 
-		g_pWindow = SDL_CreateWindow("Game Framework", // 윈도우 이름 바꾸기
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			1024, 768, // 윈도우 크기 바꾸기
-			SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED); // 과제2. 윈도우 생성 다르게 해보기
-
-		if (g_pWindow != 0)
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		else
-			return 1;
+	if (init("Breaking Up HelloSDL",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		640, 480,
+		SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
+	}
+	else
+	{
+		return 1;
 	}
 
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 0); // 과제1. 윈도우 바탕화면 파란색
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
+	while (g_bRunning)
+	{
+		input();
+		update();
+		render();
+	}
 
-	SDL_Delay(10000); // 과제3. 10초후 종료
 	SDL_Quit();
-
 	return 0;
+}
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+
+		g_pWindow = SDL_CreateWindow(
+			title, xpos, ypos, height, width, flags);
+
+		if (g_pWindow != 0) {
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+
+	}
+	else {
+		return false;
+	}
+
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+	return true;
+}
+
+void input()
+{
+	
+}
+
+void update()
+{
+	g_bRunning = rand() % 2;
+	SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
+	SDL_Delay(1000);
+}
+
+void render()
+{
+	SDL_RenderClear(g_pRenderer); // 백버퍼 초기화
+	SDL_RenderPresent(g_pRenderer); // 백버퍼 스위치
 }
