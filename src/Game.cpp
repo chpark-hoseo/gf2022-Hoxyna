4a7166c4b6c919da03cba0bd183dda41ae3ad12e
Hoxyna <frankim0622@naver.com>
2022-09-23 14:21:56 +0900

    SDL 기본 그리기 실습 완료

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,21 +1,95 @@
 #include "Game.h"
 
-Game* g_game = 0;
-
-int main(int argc, char* args[])
+bool Game::init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
-	g_game = new Game();
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        m_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, width, height, flags);
+        if (m_pWindow != 0) {
+            m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
-	g_game->init("Game Class", 100, 100, 640, 480, 0);
+            if (m_pRenderer != 0) {
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+            }
+            else {
+                return false; // 랜더러 생성 실패
+            }
+        }
+        else {
+            return false; // 윈도우 생성 실패
+        }
+    }
+    else {
+        return false; // SDL 초기화 실패
+    }
 
-	while (g_game->running())
-	{
-		g_game->handleEvents();
-		g_game->update();
-		g_game->render();
-	}
+    m_bRunning = true;
 
-	g_game->clean();
+    SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+    if (pTempSurface == NULL) return false; // BMP 불러오기 실패
 
-	return 0;
+    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+    SDL_FreeSurface(pTempSurface);
+
+    SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+    m_destinationRectangle.w = m_sourceRectangle.w;
+    m_destinationRectangle.h = m_sourceRectangle.h;
+
+    m_destinationRectangle.x = m_sourceRectangle.x = 0;
+    m_destinationRectangle.y = m_sourceRectangle.y = 0;
+
+
+    return true;
+}
+
+void Game::update()
+{
+    //m_destinationRectangle.x++;
+    //SDL_Delay(10);
+}
+
+void Game::render()
+{
+    SDL_RenderClear(m_pRenderer);
+    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+    SDL_RenderPresent(m_pRenderer);
+}
+
+bool Game::running()
+{
+    return m_bRunning;
+}
+
+void Game::handleEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) // 과제 1. if 를 while 로 바꾸는 이유는 계속해서 event값을 확인하고
+        // event.type 에 보류중인 이벤트가 없을 시 while문을 빠져나오도록 하기 위해. 
+        // if 로 사용하면 main문이 다 돌고나서 다시 검사함으로, 추후 반응이 느려질 것 같다.
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+        case SDL_KEYDOWN:
+            printf("%d 키가 눌렸어요!\n", event.key.keysym.sym);
+            break;
+        case SDL_KEYUP:
+            printf("%d 키가 때졌어요!\n", event.key.keysym.sym);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Game::clean()
+{
+    SDL_DestroyWindow(m_pWindow);
+    SDL_DestroyRenderer(m_pRenderer);
+    SDL_Quit();
 }
--- src/Game.h
+++ src/Game.h
@@ -22,80 +22,10 @@ public:
 private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
+
+    SDL_Texture* m_pTexture;
+    SDL_Rect m_sourceRectangle;
+    SDL_Rect m_destinationRectangle;
+
 	bool m_bRunning;
 };
-
-bool Game::init(const char* title, int xpos, int ypos, int height, int width, int flags)
-{
-    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-        m_pWindow = SDL_CreateWindow(
-            title, xpos, ypos, width, height, flags);
-        if (m_pWindow != 0) {
-            m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
-
-            if (m_pRenderer != 0) {
-                SDL_SetRenderDrawColor(
-                    m_pRenderer, 255, 255, 255, 255);
-            }
-            else {
-                return false; // 랜더러 생성 실패
-            }
-        }
-        else {
-            return false; // 윈도우 생성 실패
-        }
-    }
-    else {
-        return false; // SDL 초기화 실패
-    }
-
-    m_bRunning = true;
-    return true;
-}
-
-void Game::update()
-{
-
-}
-
-void Game::render()
-{
-    SDL_RenderClear(m_pRenderer);
-    SDL_RenderPresent(m_pRenderer);
-}
-
-bool Game::running()
-{
-    return m_bRunning;
-}
-
-void Game::handleEvents()
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) // 과제 1. if 를 while 로 바꾸는 이유는 계속해서 event값을 확인하고
-        // event.type 에 보류중인 이벤트가 없을 시 while문을 빠져나오도록 하기 위해. 
-        // if 로 사용하면 main문이 다 돌고나서 다시 검사함으로, 추후 반응이 느려질 것 같다.
-    {
-        switch (event.type)
-        {
-        case SDL_QUIT:
-            m_bRunning = false;
-            break;
-        case SDL_KEYDOWN:
-            printf("%d 키가 눌렸어요!\n", event.key.keysym.sym);
-            break;
-        case SDL_KEYUP:
-            printf("%d 키가 때졌어요!\n", event.key.keysym.sym);
-            break;
-        default:
-            break;
-        }
-    }
-}
-
-void Game::clean()
-{
-    SDL_DestroyWindow(m_pWindow);
-    SDL_DestroyRenderer(m_pRenderer);
-    SDL_Quit();
-}
